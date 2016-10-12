214a398edcc3584a88e4db50c4b6beeadd4e4f89
sainy <sainy@yonsei.ac.kr>
2016-10-12 16:24:36 +0900

    pre-view post-list page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
